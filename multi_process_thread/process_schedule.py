81569cec284b2b1b0ede604b6be2ab8c39bc6de5
minicloudsky <1397991131@qq.com>
2020-12-20 11:26:57 +0800

    threads

--- multi_process_thread/process_schedule.py
+++ multi_process_thread/process_schedule.py
@@ -3,11 +3,11 @@ from multiprocessing import Pool, cpu_count
 import os
 import time
 import math
+import logging
 
 
 def task_func(data):
-    for i in data:
-        print("{} pid: {}".format(i, os.getpid()))
+    print("pid {} data {}".format(os.getpid(), data))
 
 
 def long_time_task(i):
@@ -19,42 +19,36 @@ def long_time_task(i):
 class RunProcessTask():
     cpu_num = cpu_count()
 
-    def __init__(self, task_data, func, multiple=1):
+    def __init__(self, task_data, run_task_func, proceee_cpu_time=1):
         """
         100个url，2个cpu，进程数是CPU数的3倍，每个进程分 100/(2*3)
         """
-        process_num = self.cpu_num * multiple
-        print("func: ", func)
+        # 开启的进程数
+        process_num = self.cpu_num * proceee_cpu_time
         task_data = self.split_data(task_data, int(math.ceil(
-            len(task_data)/(multiple*self.cpu_num))))
+            len(task_data) / process_num)))
         pool = Pool(process_num)
-        print(task_data)
+        result = []
+        print(run_task_func)
         for data in task_data:
-            pool.apply_async(func, args=(data))
+            result.append(pool.map_async(run_task_func, data))
         pool.close()
         pool.join()
-        print("finish task .")
 
     def split_data(self, data, per_size=10):
         target_list = []
-        for i in range(int(math.ceil(len(data)/per_size))):
+        for i in range(int(math.ceil(len(data) / per_size))):
             start = i * per_size
-            if start+per_size < len(data):
+            if start + per_size < len(data):
                 end = start + per_size
             else:
                 end = len(data)
-            print("start {} end {}".format(start, end))
             per_list = data[start:end]
             if per_list:
                 target_list.append(per_list)
         return target_list
 
-    def long_time_task(self, i):
-        print('子进程: {} - 任务{}'.format(os.getpid(), i))
-        time.sleep(2)
-        print("结果: {}".format(8 ** 20))
-
 
 if __name__ == '__main__':
     data = [x for x in range(100)]
-    task = RunProcessTask(data, long_time_task, 1)
+    task = RunProcessTask(data, task_func, 2)
